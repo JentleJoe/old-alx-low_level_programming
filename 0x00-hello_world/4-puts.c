aaa505968afb4101e9caf72ddb480cb018ab2463
JentleJoe <aigbokhaebholojoshua@gmail.com>
2022-09-08 21:47:05 -0700

    correcte with betty style

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,12 @@
 # include <stdio.h>
-
-	int main()
+/**
+ * main - prints the text
+ * return: 0
+ */
+	int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	return(0);
+	return (0);
 }
 
