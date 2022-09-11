672ace427b765a2d921902bfccb6421711290dbf
JentleJoe <aigbokhaebholojoshua@gmail.com>
2022-09-11 11:07:45 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - entry point for all data
+ *
+ * Return: return 0 if succesful
+ */
+
+	int main(void)
+	{
+		printf("with proper grammer, but the outcome is a piece of art,\n");
+
+		return (0);
+	}
