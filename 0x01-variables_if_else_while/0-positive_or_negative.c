cc064c41b3fbefaa5df26a204570cc62f789034d
JentleJoe <aigbokhaebholojoshua@gmail.com>
2022-09-12 02:42:10 -0700

    corrected code errors

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,11 +1,12 @@
-#include <stdlib.h>
-
-#include <time.h>
-
 /**
  * main - assigns a random number to n
  * Return: return to zero
  */
+
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
 
 int main(void)
 
@@ -19,14 +20,17 @@ int main(void)
 
 		n = rand() - RAND_MAX / 2;
 
-		if (n > 0) {
+		if (n > 0)
+		{
 			printf("%d is positive\n", n);
 		}
-		else if (n = 0) {
+		else if (n == 0)
+		{
 			printf("%d is zero\n", n);
 		}
-		else if (n < 0) {
-			printf("%d is negative\n", n)
+		else if (n < 0)
+		{
+			printf("%d is negative\n", n);
 		};
 
 		return (0);
