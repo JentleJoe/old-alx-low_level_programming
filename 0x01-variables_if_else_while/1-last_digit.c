631bc149e1187a0950b21108764fe28a432dd89c
JentleJoe <aigbokhaebholojoshua@gmail.com>
2022-09-12 04:46:51 -0700

    give details of the last digits of numbers

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,10 +1,10 @@
 #include <stdlib.h>
-
 #include <time.h>
+#include <stdio.h>
 
 /**
  * main - assigns a random number to n
- * Return: return to zero
+ * Return: returns to zero if successful
  */
 
 
@@ -14,21 +14,26 @@ int main(void)
 {
 
 		int n;
-
+		int digit;
 
 
 		srand(time(0));
 
 		n = rand() - RAND_MAX / 2;
 
-	if (n % 10 > 5) {
-		printf("and is greater than 5\n");
+	digit = n % 10;
+
+	if (digit > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, digit);
 	}
-	else if (n % 10 == 0) {
-		printf("and is 0\n");
-       	}
-	else if (n % 10 < 6 && n % 10 != 0) {
-		printf("and is less than 6 and not 0\n");
+	else if (digit == 0)
+	{
+		printf("Last digit of %d is %d and is equal to zero \n", n, digit);
+	}
+	else if (digit > 0 < 6)
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0 \n", n, digit);
 	}
 
 	return (0);
--- 0x01-variables_if_else_while/README.md
+++ 0x01-variables_if_else_while/README.md
@@ -1,2 +1,2 @@
 Program that tells if a number is positive, negative or zero
-program that prints the last digit of the number stored in the variable
+Program that tells if the last digit of a number is greater than 5 or less than 6 but not 0 or is 0
