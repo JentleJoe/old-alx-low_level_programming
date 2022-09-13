f1aad6853b4b0dda0e73f3d1fb13803e4df0aa2a
JentleJoe <aigbokhaebholojoshua@gmail.com>
2022-09-12 20:47:21 -0700

    edited return value

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 
 /**
  * main - prints the text
- * Return: 0 if exited properly, non-zero otherwise
+ * Return: 1 if exited properly, non-zero otherwise
  */
 	int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	return (0);
+	return (1);
 }
