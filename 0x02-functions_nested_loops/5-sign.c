367a2fdb03c79476b41d45e7480ea85d68d9d1e1
JentleJoe <aigbokhaebholojoshua@gmail.com>
2022-09-14 16:56:46 -0700

    removed comments due to multiple betty documentation style error

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -5,7 +5,6 @@
  * @n: variable used to run the check
  *
  * Return: 1 if positive, returns -1 if negative and returns 0 if zero
- *
  */
 
 int print_sign(int n)
@@ -15,14 +14,6 @@ int print_sign(int n)
 		_putchar('+');
 		return (1);
 	}
-
-/**
-*	else if (n == 0)
-*	{
-*		_putchar('0');
-*		return (0);
-*	}
-*/
 
 	else if (n < 0)
 	{
