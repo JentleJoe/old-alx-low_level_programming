eb5dc2a1dba7e0c2bccc3f17284b04df76c38536
JentleJoe <aigbokhaebholojoshua@gmail.com>
2022-09-16 01:39:04 -0700

    program that checks for digit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+  * _isdigit - Main entry point to check for digit
+  * @c: variable that cheecks for digit
+  *
+  *Return: 1 if c is a digit, 0 otehrwise
+  */
+
+
+int _isdigit(int c)
+{
+
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+
+	return (0);
+
+}
+
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -1,1 +1,3 @@
 0.	Program that checks for uppercase character
+
+1.	Program that checks for a digit (0 through 9), returns 1 if c is a digit, 0 otherwise
