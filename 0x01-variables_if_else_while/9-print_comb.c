f6343852e9a663c14a83dc6cd39a673110f99a97
JentleJoe <aigbokhaebholojoshua@gmail.com>
2022-09-18 23:26:53 -0700

    made space after 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,13 +8,17 @@
 {
 	int i = 0;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 10; i++)
 	{
 		putchar (i + '0');
+		if (i == 9)
+		{
+			break;
+		}
 		putchar (',');
 		putchar (' ');
 	}
-		putchar ('9');
+	putchar (' ');
 
 
 	return (0);
