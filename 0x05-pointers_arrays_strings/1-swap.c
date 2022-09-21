4338c6481650866bf534b9ec08be58b7e5556a1b
JentleJoe <aigbokhaebholojoshua@gmail.com>
2022-09-20 21:44:39 -0700

    updated task 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * swap_int - Function to swap integer
+ * @a:  First integer
+ * @b: second integer
+ *
+ * Return - void *
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	
+	*a = *b;
+	*b = c;
+}
