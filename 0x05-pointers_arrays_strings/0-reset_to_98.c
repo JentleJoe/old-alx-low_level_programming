92127d3c848438d37266f22a6e4c524f1f136c70
JentleJoe <aigbokhaebholojoshua@gmail.com>
2022-09-20 22:20:47 -0700

    corrected documentation error

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,39 +2,9 @@
 #include "main.h"
 
 /**
- * Main - Entry Point
  * reset_to_98 - updates the value of n
  * @n: pointer
  *
- */
-
-/**
- *	old code
- *
- *int main(void)
- *{
- *	int n;
- *	int *p;
- *
- *	n = 402;
- *	p = &n;
- *
- *	using printf
- *	printf ("n=%d", n);
- *	*p = 98;
- *	printf ("n=%d", n);
- *
- *	using  _putchar
- *	_putchar ('n');
- *	_putchar ('=');
- *	_putchar (n + '0');
- *	_putchar ('\n');
- *
- *	*p = 98;
- *	_putchar (p + '0');
- *
- *	_putchar ('\n');
- *}
  */
 
 void reset_to_98(int *n)
