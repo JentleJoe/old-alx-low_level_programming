54dc0fb57675f3403486ed01e739a359a218ca78
JentleJoe <aigbokhaebholojoshua@gmail.com>
2022-09-23 21:31:25 +0000

    pre task template

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat:
+ *
+ * Return: concartenation result
+ */
+
+char *_strcat(char *dest, char *src)
+{
+
+/*	dest[(strlen(dest) - 1)];
+	src[(strlen(src) - 1)];
+
+	return (_strcat(dest, src));
+*/
+}
+
+
