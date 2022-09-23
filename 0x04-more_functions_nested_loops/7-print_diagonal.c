316001e69a73abec9ecfa7863e2409b5a93147e4
JentleJoe <aigbokhaebholojoshua@gmail.com>
2022-09-23 15:36:14 +0000

    draw a diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * print_diagonal - draws a staight in the terminal
+ * @n: user input that determines the number of times ' _ ' is printed
+ *
+ * Retutn - void
+ */
+
+void print_diagonal(int n)
+{
+	int i = 0;
+	int a ;
+
+	if (n > 0)
+	{
+		while (i < n)
+		{
+			_putchar ('\\');
+
+			if (i == (n-1))
+			{
+				break;
+			}
+			_putchar ('\n');
+
+			for (a = 0; a <= i; a++)
+			{
+				_putchar (' ');
+			}
+			i++;
+		}
+		_putchar ('\n');
+	}
+	else
+	{
+		_putchar ('\n');
+	}
+
+}
--- 0x04-more_functions_nested_loops/README.md
+++ 0x04-more_functions_nested_loops/README.md
@@ -11,3 +11,5 @@
 5.	Function that prints 10 times the numbers, from 0 to 14, followed by a new line
 
 6.	Program that draws a straight line in the terminal
+
+7.	Function that draws a diagonal line on the terminal
