d1d70ab084f309faa12ba68262535a8823d45c6c
JentleJoe <aigbokhaebholojoshua@gmail.com>
2022-10-10 19:56:28 +0000

    added other files, check readme for discription

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+#ifndef FILE
+
+#include <stdio.h>
+
+#endif
+
+/**
+ * main - prints file name
+ * __FILE__ - macro used to print file name
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,11 @@
+#ifndef STDLIB_H
+
+#include <stdlib.h>
+
+#endif
+
+#ifndef ABS(x)
+
+#define ABS(x) abs(x)
+
+#endif
