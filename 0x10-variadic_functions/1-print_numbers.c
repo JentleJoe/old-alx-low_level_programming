20d8b3318e16994077c3d3201bed604fa9241a08
JentleJoe <aigbokhaebholojoshua@gmail.com>
2022-10-14 04:50:21 +0000

    updated

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,44 +1,28 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
 
 /**
- * print_numbers - print numbers
+ * print_numbers - prints numbers given as parameters
  * @separator: string to be printed between numbers
- * @n: numer of integers passed
- *
- * Return: Always 0
+ * @n: number of integers passed to the function
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
-	va_list ap;
-	unsigned int i, num;
+	unsigned int i;
+	va_list list;
 
-	va_start(ap, n);
+	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		if (n == 0)
-		{
-			break;
-		}
-		num = va_arg(ap, int);
-		if (separator == NULL)
-		{
-			printf("%d", num);
-		}
+		if (!separator)
+			printf("%d", va_arg(list, int));
+		else if (separator && i == 0)
+			printf("%d", va_arg(list, int));
 		else
-		{
-			if (i == n - 1)
-			{
-				printf("%d", num);
-				break;
-			}
-			printf("%d%s ", num, separator);
-		}
+			printf("%s%d", separator, va_arg(list, int));
 	}
-	va_end(ap);
+
+	va_end(list);
 
 	printf("\n");
 }
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -1,45 +1,33 @@
 #include "variadic_functions.h"
-#include <stdarg.h>
-#include <stdio.h>
 
 /**
- * print_strings - print strings
- * @separator: strings to be printed
- * @n: number of strings to print
- *
- * Return: Always 0
+ * print_strings - prints strings
+ * @separator: separator between strings
+ * @n: number of arguments
  */
-
 void print_strings(const char *separator, const unsigned int n, ...)
 {
-	va_list ap;
 	unsigned int i;
-	char *c;
+	char *str;
 
-	va_start(ap, n);
+	va_list list;
+
+	va_start(list, n);
 
 	for (i = 0; i < n; i++)
 	{
-		if (n == 0)
-		{
-			break;
-		}
-		c = va_arg(ap, char *);
-		if (c == NULL)
-		{
-			printf("nil");
-		}
+		str = va_arg(list, char *);
+		if (!str)
+			str = "(nil)";
+		if (!separator)
+			printf("%s", str);
+		else if (separator && i == 0)
+			printf("%s", str);
 		else
-		{
-			if (i == n - 1)
-			{
-				printf("%s", c);
-				break;
-			}
-
-			printf("%s%s", c, separator);
-		}
+			printf("%s%s", separator, str);
 	}
-	va_end(ap);
+
 	printf("\n");
+
+	va_end(list);
 }
