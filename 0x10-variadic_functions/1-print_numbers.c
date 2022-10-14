35e3e8d080e53c83454c5e8738ac9dc7b241cca2
JentleJoe <aigbokhaebholojoshua@gmail.com>
2022-10-14 03:59:46 +0000

    added condition for n = 0

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -19,6 +19,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 
 	for (i = 0; i < n; i++)
 	{
+		if (n == 0)
+		{
+			break;
+		}
 		num = va_arg(ap, int);
 		if (separator == NULL)
 		{
